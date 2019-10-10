cbd5576af82eb6c9f65fb0923d63ddc8bbb14ed2
NarohC <NarohCramhen@gmail.com>
2019-10-10 11:57:30 -0600

    correcciones a pasaporte

--- Passport/Passport/CountriesTableViewController.swift
+++ Passport/Passport/CountriesTableViewController.swift
@@ -35,7 +35,7 @@ class CountriesTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell (withIdentifier: "reuseIdentifier", for: indexPath) as UITableViewCell
+        let cell = tableView.dequeueReusableCell (withIdentifier: "reuseIdentifier")!
         let country = countries[indexPath.row]
         cell.textLabel!.text = country
         return cell
